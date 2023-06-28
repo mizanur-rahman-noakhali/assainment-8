cf44601cbb8cffef19a5b6c26dc2c3fe1ed23c45
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2023-06-28 15:28:22 +0600

    material ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { Button } from '@material-ui/core';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Button>Primary</Button>
+      <Button variant="contained">Contained</Button>
+<Button variant="contained" disabled>
+  Disabled
+</Button>
+<Button variant="contained" href="#contained-buttons">
+  Link
+</Button>
     </div>
   );
 }
