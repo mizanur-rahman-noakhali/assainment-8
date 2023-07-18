eb364763c4d6eea6fba530329136cd6908f95265
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2023-07-18 13:11:21 +0600

    add comment

--- src/PostDetail/PostDetail.js
+++ src/PostDetail/PostDetail.js
@@ -10,11 +10,22 @@ const PostDetail = () => {
        .then(res=>res.json())
        .then(data=>setPost(data))
   },[])
+   const [comment,setComment]=useState({});
+    useEffect(()=>{
+      const url=`https://jsonplaceholder.typicode.com/comments/${postId}`;
+      fetch(url)
+      .then(res=>res.json())
+      .then(data=> setComment(data))
+    });
+
   return (
     <div>
       <h3>This is Post Details:{postId}</h3>
       <p>Title:{post.title}</p>
       <p>body:{post.body}</p>
+      <p>commenter:{comment.name}</p>
+      <p>comment:{comment.email}</p>
+      <p>comment:{comment.body}</p>
     </div>
   );
 };
